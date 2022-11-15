0c013d3a671025dbc90480412fbad3478b283904
Deep0902 <deeptank09@gmail.com>
2022-11-15 12:41:52 +0530

    Add project files.

---
+++ Calculator/Calc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculator
+{
+    internal class Calc
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
